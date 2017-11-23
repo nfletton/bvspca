182fb7bd5c90581755f27acdc4f994cf4f13a3c3
Nigel Fletton <nigel@thebluehut.com>
2017-11-23 11:40:04 -0700

    Fix spacing issue on age presentation

--- bvspca/animals/templatetags/animal_tags.py
+++ bvspca/animals/templatetags/animal_tags.py
@@ -14,12 +14,12 @@ def to_years_an_months(months):
     result = ''
     if whole_years >= 1:
         if whole_years == 1:
-            result = '1 Year'
+            result = '1 Year '
         else:
             result = '{} Years '.format(whole_years)
     if remaining_months >= 1:
         if months == 1:
             result += '1 Month'
         else:
-            result += '{} Months '.format(remaining_months)
+            result += '{} Months'.format(remaining_months)
     return result
