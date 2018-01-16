591db616f748b4ab42fe1914790120f079a539ef
Nigel Fletton <nigel@thebluehut.com>
2018-01-15 17:08:26 -0700

    Add staging to allowed hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -16,7 +16,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # ------------------------------------------------------------------------------
 # Hosts/domain names that are valid for this site
 # See https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['bowvalleyspca.org', ])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['www.bowvalleyspca.org', 'www.bvspca.bluehut.ca'])
 # END SITE CONFIGURATION
 
 INSTALLED_APPS += ['gunicorn', ]
