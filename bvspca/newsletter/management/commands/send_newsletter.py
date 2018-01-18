5d166a66828ced54655e9c9bd1454d029ed627ad
Nigel Fletton <nigel@thebluehut.com>
2018-01-18 16:29:40 -0700

    Weekly animal update email feature completed

--- bvspca/newsletter/management/commands/send_newsletter.py
+++ bvspca/newsletter/management/commands/send_newsletter.py
@@ -1,9 +1,13 @@
 import logging
-from datetime import date
+import datetime
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
+from django.template.loader import render_to_string
 from mailchimp3 import MailChimp
+from wagtail.wagtailcore.models import Site
+
+from bvspca.animals.models import Animal
 
 
 class Command(BaseCommand):
@@ -21,7 +25,7 @@ class Command(BaseCommand):
             },
             "settings": {
                 "subject_line": settings.MAILCHIMP_SUBJECT,
-                "title": '{} ({:%Y-%m-%d})'.format(settings.MAILCHIMP_SUBJECT, date.today()),
+                "title": '{} ({:%Y-%m-%d})'.format(settings.MAILCHIMP_SUBJECT, datetime.date.today()),
                 "from_name": settings.MAILCHIMP_FROM_NAME,
                 "reply_to": settings.MAILCHIMP_REPLY_TO,
                 "template_id": int(settings.MAILCHIMP_TEMPLATE_ID),
@@ -33,22 +37,51 @@ class Command(BaseCommand):
             self.logger.error('Failed to create MailChimp weekly update campaign: %s', e)
             raise
 
-        content = {
-            'template': {
-                'id': int(settings.MAILCHIMP_TEMPLATE_ID),
-                'sections': {
-                    'body': '<h2>hello world</h2>',
+        try:
+            since_date = datetime.date.today() - datetime.timedelta(14)
+            site = Site.objects.get(pk=2)
+            site_url = site.root_url
+            content = {
+                'template': {
+                    'id': int(settings.MAILCHIMP_TEMPLATE_ID),
+                    'sections': {
+                        'arrived': self.new_animals_list(since_date, site_url),
+                        'adopted': self.adopted_animals_list(since_date, site_url),
+                    },
                 },
-            },
-        }
+            }
+        except Exception as e:
+            self.logger.error('Failed to prepared date for weekly animal update')
+            raise
+
         try:
             client.campaigns.content.update(campaign['id'], content)
         except Exception as e:
             self.logger.error('Failed to update MailChimp campaign (%s) with content: %s', campaign['id'], e)
             raise
+
         try:
             response = client.campaigns.actions.send(campaign['id'])
             if response:
                 self.logger.error('%s', response)
         except Exception as e:
             self.logger.error('Failed to send MailChimp campaign (%s): %s', campaign['id'], e)
+
+    @staticmethod
+    def new_animals_list(since, root_url):
+        new_animals = Animal.objects\
+            .filter(adoption_date__isnull=True, last_intake_date__gte=since)\
+            .live()\
+            .order_by('last_intake_date')
+        return render_to_string(
+            'newsletter/animal_list.html',
+            {'animals': new_animals, 'root_url': root_url}
+        )
+
+    @staticmethod
+    def adopted_animals_list(since, root_url):
+        adopted_animals = Animal.objects.filter(adoption_date__gte=since).live().order_by('adoption_date')
+        return render_to_string(
+            'newsletter/animal_list.html',
+            {'animals': adopted_animals, 'root_url': root_url}
+        )
