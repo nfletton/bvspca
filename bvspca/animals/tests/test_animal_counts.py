6999eaab7541e927418af27dab43ce023a999cae
Nigel Fletton <nigel@thebluehut.com>
2018-03-05 16:25:43 -0700

    Fix up animal count tests

--- bvspca/animals/tests/test_animal_counts.py
+++ bvspca/animals/tests/test_animal_counts.py
@@ -1,23 +1,29 @@
 import pytest
-from wagtail.wagtailcore.models import Page, Site
 
 from bvspca.animals.management.commands.sync_petpoint_data import Command
 from bvspca.animals.models import AnimalCountSettings
 
 
-@pytest.fixture(scope="module")
-def animal_settings():
-    root_page = Page(title='test', path='test', depth=1)
-    root_page.save()
-    site = Site(hostname='www.example.com', root_page=root_page)
-    site.save()
-    count_settings = AnimalCountSettings(site_id=site.id)
-    count_settings.save()
-    return count_settings
+@pytest.mark.django_db
+def test_decrement_animal_count():
+    animal_settings = AnimalCountSettings.objects.get(pk=1)
+    animal_settings.cats_adopted = 22
+    animal_settings.cats_rescued = 44
+    animal_settings.dogs_adopted = 55
+    animal_settings.dogs_rescued = 66
+    animal_settings.save()
+
+    Command.increment_animal_count('Cat', 'adopted', -1)
+    animal_settings.refresh_from_db()
+    assert animal_settings.cats_adopted == 21
+    Command.increment_animal_count('Dog', 'rescued', -1)
+    animal_settings.refresh_from_db()
+    assert animal_settings.dogs_rescued == 65
 
 
-@pytest.mark.django_db(transaction=False)
-def test_increment_animal_count(animal_settings):
+@pytest.mark.django_db
+def test_increment_animal_count():
+    animal_settings = AnimalCountSettings.objects.get(pk=1)
     for _ in range(1, 100):
         Command.increment_animal_count('Cat', 'adopted')
         animal_settings.refresh_from_db()
@@ -34,19 +40,3 @@ def test_increment_animal_count(animal_settings):
         Command.increment_animal_count('Dog', 'rescued')
         animal_settings.refresh_from_db()
     assert animal_settings.dogs_rescued == 49
-
-
-@pytest.mark.django_db(transaction=False)
-def test_decrement_animal_count(animal_settings):
-    animal_settings.cats_adopted = 22
-    animal_settings.cats_rescued = 44
-    animal_settings.dogs_adopted = 55
-    animal_settings.dogs_rescued = 66
-    animal_settings.save()
-
-    Command.increment_animal_count('Cat', 'adopted', -1)
-    animal_settings.refresh_from_db()
-    assert animal_settings.cats_adopted == 21
-    Command.increment_animal_count('Dog', 'rescued', -1)
-    animal_settings.refresh_from_db()
-    assert animal_settings.dogs_rescued == 65
---
+++ conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from wagtail.wagtailcore.models import Page, Site
+
+from bvspca.animals.models import AnimalCountSettings
+
+
+@pytest.fixture(scope='session')
+def django_db_setup(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        root_page = Page(title='test', path='test', depth=1)
+        root_page.save()
+        site = Site(hostname='www.example.com', root_page=root_page)
+        site.save()
+        count_settings = AnimalCountSettings(site_id=site.id)
+        count_settings.save()
+
