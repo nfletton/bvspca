e3defd867c3eb9aa4e560971c347c9f0fa0deccb
Nigel Fletton <nigel@thebluehut.com>
2018-07-16 14:33:22 -0600

    Reinstate deletion of social media queue entries

--- bvspca/social/management/commands/post_social_media.py
+++ bvspca/social/management/commands/post_social_media.py
@@ -16,6 +16,7 @@ class Command(BaseCommand):
     logger = logging.getLogger('bvspca.social')
 
     def handle(self, *args, **options):
+        SocialMediaQueue.objects.delete_old_entries()
         queued_entries = SocialMediaQueue.objects.all().order_by('priority', '-date')
         for entry in queued_entries:
             # post first page ready to post
